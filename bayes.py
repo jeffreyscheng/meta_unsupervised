5557e41ad740ae832b4533ebae105e7fe1f97eb3
Jeffrey Cheng <jeffch@wharton.upenn.edu>
2018-04-05 14:08:40 -0400

    fixed bayes bounds

--- bayes.py
+++ bayes.py
@@ -1,8 +1,8 @@
 from bayes_opt import BayesianOptimization
 from single_single import *
 
-param_dict = {'mid1': (20, 800), 'mid2': (20, 800), 'meta_mid': (2, 10), 'meta_batch_size': (0, 10000),
-              'learning_rate': (0, 0.001), 'meta_rate': (0, 0.001)}
+param_dict = {'mid1': (20, 800), 'mid2': (20, 800), 'meta_mid': (2, 10), 'meta_batch_size': (1, 10000),
+              'learning_rate': (0.000001, 0.001), 'meta_rate': (0.000001, 0.001)}
 bayes = BayesianOptimization(train_model, param_dict)
 
 bayes.explore({'mid1': [starting_learner_mid1], 'mid2': [starting_learner_mid2], 'meta_mid': [starting_meta_mid],
