1b0a6027833c245ddca7a63e4b747da553850f82
Jeffrey Cheng <jeffch@wharton.upenn.edu>
2018-04-10 17:22:51 -0400

    added separate

--- net_components.py
+++ net_components.py
@@ -34,6 +34,7 @@ class Meta(nn.Module):
         out = self.fc1(x)
         out = self.relu(out)
         out = self.fc2(out)
+        out = self.relu(out)
         return out
 
 
@@ -125,3 +126,106 @@ class SingleNet(nn.Module):
                         shift = self.get_update(input_to_neuron, neuron_weight, output_from_neuron)
                         # print(new_weight)
                         layer.data[output_index, input_index] -= shift * rate / epoch
+
+
+# Template for Single Structure
+class UnsupervisedNet(nn.Module):
+
+    def __init__(self, input_size, hidden, output_size, meta_weight, batch_size):
+        super(UnsupervisedNet, self).__init__()
+        self.relu = nn.ReLU()
+        self.fc1 = nn.Linear(input_size, hidden)
+        self.fc2 = nn.Linear(hidden, output_size)
+        self.batch_size = batch_size
+        self.impulse = None
+        self.meta_weight = meta_weight
+        self.metadata = {}
+
+        self.param_state = self.state_dict(keep_vars=True)
+        keys = list(self.param_state.keys())
+
+        def is_weight_param(param):
+            return (".weight" in param) and ("meta" not in param)
+
+        self.weight_params = [key for key in keys if is_weight_param(key)]
+
+    # get new weight
+    def get_update(self, v_i, w_ij, v_j):
+        inputs = Variable(torch.Tensor([v_i, w_ij, v_j]), requires_grad=True)
+        # print("break")
+        return self.meta_weight.forward(inputs).data[0]
+
+    def forward(self, x):
+        self.metadata = {}
+        out = x
+        self.impulse = [out]
+        out = self.fc1(out)
+        out = self.relu(out)
+        self.impulse.append(out)
+        out = self.fc2(out)
+        out = self.relu(out)
+        self.impulse.append(out)
+        return out
+
+    # @timeit
+    # compute output and propagate Hebbian updates
+    # takes roughly 10 ms per
+    def update(self, rate, epoch, change_weights=True):
+        # print(weight_params)
+        if len(self.weight_params) != len(self.impulse) - 1:
+            print("Keys:" + str(len(self.weight_params)))
+            print(self.weight_params)
+            print("Impulse:" + str(len(self.impulse)))
+            print(self.impulse)
+            raise ValueError("Num keys not 1 less than num impulses")
+        for i in range(0, len(self.weight_params)):
+            layer = self.param_state[self.weight_params[i]]
+            # print(layer)
+            input_layer = self.impulse[i]
+            output_layer = self.impulse[i + 1]
+            # print(input_layer.size())
+            # print(output_layer.size())
+            # print(layer.size())
+            # stack_dim = self.batch_size, layer.size()[0], layer.size()[1]
+            # input_stack = input_layer.unsqueeze(1).expand(stack_dim)
+            # output_stack = output_layer.unsqueeze(2).expand(stack_dim)
+            # weight_stack = layer.unsqueeze(0).expand(stack_dim)
+            # meta_inputs = torch.stack((input_stack, weight_stack, output_stack), dim=3)
+            # print(meta_inputs.size())
+
+            input_stack = input_layer.repeat(output_layer.size(1), 1)
+            output_stack = output_layer.repeat(input_layer.size(1), 1).t()
+            meta_inputs = torch.stack((input_stack, layer, output_stack), dim=2)
+            self.metadata[self.weight_params[i]] = meta_inputs
+            # layer.data = torch.stack([self.meta_weight() for i, x_i in enumerate(torch.unbind(x, dim=0), 0)], dim=0)
+            # TODO: vectorize with apply_()
+            if change_weights:
+                for input_index in range(0, len(input_layer)):
+                    for output_index in range(0, len(output_layer)):
+                        # print(input_layer.data)
+                        input_to_neuron = input_layer.data[0, input_index]
+                        # print(input_to_neuron)
+                        output_from_neuron = output_layer.data[0, output_index]
+                        # print(output_from_neuron)
+                        neuron_weight = layer.data[output_index, input_index]
+                        # print(neuron_weight)
+                        shift = self.get_update(input_to_neuron, neuron_weight, output_from_neuron)
+                        # print(new_weight)
+                        layer.data[output_index, input_index] -= shift * rate / epoch
+
+
+# Template for Bias and Weight Update Metalearner
+# inputs in order v_i, w_ij, v_j
+class SupervisedNet(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(SupervisedNet, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        out = self.relu(out)
+        return out
---
+++ separate.py
@@ -0,0 +1,118 @@
+from meta_framework import *
+from net_components import *
+
+
+class Separate(MetaFramework):
+    def __init__(self, name, fixed_params, variable_params_range, variable_params_init):
+        super(Separate, self).__init__(name, fixed_params, variable_params_range, variable_params_init)
+
+    def train_model(self, mid1, mid2, meta_mid, meta_batch_size, learning_rate, update_rate, bridge):
+        mid1 = math.floor(mid1)
+        mid2 = math.floor(mid2)
+        meta_mid = math.floor(meta_mid)
+        meta_batch_size = math.floor(meta_batch_size)
+        bridge = math.floor(bridge)
+        train_start_time = time.time()
+        meta_weight = Meta(meta_input, meta_mid, meta_output)
+        unsupervised_learner = UnsupervisedNet(input_size, mid1, bridge, meta_weight, learner_batch_size)
+        supervised_learner = SupervisedNet(bridge, mid2, num_classes)
+
+        # Loss and Optimizer
+        learner_criterion = nn.CrossEntropyLoss()
+        learner_optimizer = torch.optim.Adam(supervised_learner.parameters(), lr=learning_rate)
+
+        meta_criterion = nn.MSELoss()
+        meta_optimizer = torch.optim.Adam(meta_weight.parameters(), lr=learning_rate)
+
+        meta_epoch = 0
+        while time.time() - train_start_time < total_runtime:
+            meta_epoch += 1
+            for i, (images, labels) in enumerate(train_loader):
+                if time.time() - train_start_time > total_runtime:
+                    break
+                images = Variable(images.view(-1, 28 * 28))
+                labels = Variable(labels)
+
+                # Forward + Backward + Optimize
+                learner_optimizer.zero_grad()  # zero the gradient buffer
+                middle_outputs = unsupervised_learner(images)
+                outputs = supervised_learner(middle_outputs)
+                unsupervised_learner.update(update_rate, meta_epoch)
+                learner_loss = learner_criterion(outputs, labels)
+                # print(labels.data[0], ',', str(learner_loss.data[0]))
+                learner_loss.backward()
+                learner_optimizer.step()
+
+                for param in unsupervised_learner.weight_params:
+                    grad = torch.unsqueeze(unsupervised_learner.param_state[param].grad, 2)
+                    # print(grad.size())
+                    # print(learner.metadata[param].size())
+                    unsupervised_learner.metadata[param] = torch.cat((unsupervised_learner.metadata[param], grad), dim=2)
+                    cube_dim = unsupervised_learner.metadata[param].size()
+                    unsupervised_learner.metadata[param] = unsupervised_learner.metadata[param].view(cube_dim[0] * cube_dim[1], cube_dim[2])
+                all_metadata = torch.cat(list(unsupervised_learner.metadata.values()), dim=0)
+                # print(all_metadata.size())
+                metadata_size = all_metadata.size()[0]
+                # trol = time.time()
+                try:
+                    sample_idx = np.random.choice(metadata_size, meta_batch_size, replace=False)
+                    sampled_metadata = all_metadata[sample_idx, :]
+                except IndexError:
+                    print("===INDEX ERROR===")
+                    print(metadata_size)
+                    print(meta_batch_size)
+                    print(sample_idx)
+                    sampled_metadata = all_metadata
+                    print("===CLOSE===")
+                metadata_from_forward = MetaDataset(sampled_metadata)
+                meta_loader = torch.utils.data.DataLoader(dataset=metadata_from_forward,
+                                                          batch_size=meta_batch_size,
+                                                          shuffle=True)
+                for j, (triplets, grads) in enumerate(meta_loader):
+                    tock = time.time()
+                    triplets = Variable(triplets)
+                    grads = Variable(grads)
+
+                    # Forward + Backward + Optimize
+                    meta_optimizer.zero_grad()  # zero the gradient buffer
+                    meta_outputs = meta_weight(triplets)
+                    meta_loss = meta_criterion(meta_outputs, grads)
+                    # print("Meta-Loss:" + str(meta_loss))
+                    meta_loss.backward()
+                    meta_optimizer.step()
+                    # print(time.time() - tock)
+                # print("ONE FULL PASS")
+                # print(time.clock() - tick)
+                #
+                # if (i + 1) % 100 == 0:
+                # print('Epoch [%d/%d], Step [%d/%d], Loss: %.4f'
+                #       % (epoch + 1, num_epochs, i + 1, len(train_dataset) // batch_size, learner_loss.data[0]))
+                # print('Epoch [%d], Loss: %.4f' % (meta_epoch, learner_loss.data[0]))
+                # print('Took ', time.clock() - tick, ' seconds')
+                # meta_epoch += 1
+
+        # Test the Model
+        correct = 0
+        total = 0
+        for images, labels in test_loader:
+            images = Variable(images.view(-1, 28 * 28))
+            middle_outputs = unsupervised_learner(images)
+            outputs = supervised_learner(middle_outputs)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum()
+        # print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
+        return correct / total
+
+
+separate_fixed_params = {'meta_input': 3, 'meta_output': 1, 'input_size': 784, 'num_classes': 10,
+                         'learner_batch_size': 1}
+separate_params_range = {'mid1': (20, 800), 'mid2': (20, 800), 'meta_mid': (2, 10), 'meta_batch_size': (1, 10000),
+                         'learning_rate': (0.000001, 0.001), 'update_rate': (0.000001, 0.001), 'bridge': (20, 800)}
+separate_params_init = {'mid1': [400, 20], 'mid2': [200, 20],
+                        'meta_mid': [5, 10], 'meta_batch_size': [100, 2337],
+                        'learning_rate': [0.0001, 0.00093], 'update_rate': [0.0001, 0.00087], 'bridge': [300, 20]}
+
+separate_frame = Separate('separate', separate_fixed_params, separate_params_range, separate_params_init)
+separate_frame.optimize(1)
+separate_frame.analyze()
