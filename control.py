c1f146f7a8cc6079e34c16ccf091b541b2442485
Jeffrey Cheng <jeffch@wharton.upenn.edu>
2018-04-18 03:33:00 -0400

    added batching, vectorized

--- control.py
+++ control.py
@@ -7,7 +7,7 @@ class Vanilla(MetaFramework):
     def __init__(self, name, fixed_params, variable_params_range, variable_params_init):
         super(Vanilla, self).__init__(name, fixed_params, variable_params_range, variable_params_init)
 
-    def train_model(self, mid1, mid2, meta_mid, meta_batch_size, learning_rate, update_rate):
+    def train_model(self, mid1, mid2, meta_mid, meta_batch_size, learning_rate, update_rate, learner_batch_size):
         mid1 = math.floor(mid1)
         mid2 = math.floor(mid2)
         meta_mid = math.floor(meta_mid)
@@ -15,12 +15,12 @@ class Vanilla(MetaFramework):
         train_start_time = time.time()
         meta_input = self.fixed_params['meta_input']
         meta_output = self.fixed_params['meta_output']
-        meta_weight = Meta(meta_input, meta_mid, meta_output)
+        # meta_weight = Meta(meta_input, meta_mid, meta_output)
         input_size = self.fixed_params['input_size']
         num_classes = self.fixed_params['num_classes']
-        # learner_batch_size = math.floor(learner_batch_size)
-        learner_batch_size = 1
-        learner = SingleNet(input_size, mid1, mid2, num_classes, meta_weight, learner_batch_size)
+        learner_batch_size = math.floor(learner_batch_size)
+        # learner_batch_size = 1
+        learner = SingleNet(input_size, mid1, mid2, num_classes, meta_input, meta_mid, meta_output, learner_batch_size)
 
         # MNIST Dataset
         train_dataset = dsets.MNIST(root='./data',
@@ -46,33 +46,38 @@ class Vanilla(MetaFramework):
         learner_optimizer = torch.optim.Adam(learner.parameters(), lr=learning_rate)
 
         meta_criterion = nn.MSELoss()
-        meta_optimizer = torch.optim.Adam(meta_weight.parameters(), lr=learning_rate)
+        meta_optimizer = torch.optim.Adam(list(learner.conv1.parameters()) + list(learner.conv2.parameters()),
+                                          lr=learning_rate)
 
+        tick = time.time()
         for meta_epoch in range(1, MetaFramework.num_epochs + 1):
+            if time.time() - tick > MetaFramework.time_out:
+                break
             for i, (images, labels) in enumerate(train_loader):
                 images = Variable(images.view(-1, 28 * 28))
                 labels = Variable(labels)
 
-                # Forward + Backward + Optimize
+                # Learner Forward + Backward + Optimize
                 learner_optimizer.zero_grad()  # zero the gradient buffer
                 outputs = learner(images)
                 learner.update(update_rate, meta_epoch, change_weights=False)
                 learner_loss = learner_criterion(outputs, labels)
                 print(labels.data[0], ',', str(learner_loss.data[0]))
                 learner_loss.backward()
+                # for param in learner.weight_params:
+                #     print(param)
+                #     print(learner.param_state[param].grad)
                 learner_optimizer.step()
 
+                # wrangling v_i, v_j, w_ij to go into MetaDataset
                 for param in learner.weight_params:
-                    grad = torch.unsqueeze(learner.param_state[param].grad, 2)
-                    # print(grad.size())
-                    # print(learner.metadata[param].size())
-                    learner.metadata[param] = torch.cat((learner.metadata[param], grad), dim=2)
-                    cube_dim = learner.metadata[param].size()
-                    learner.metadata[param] = learner.metadata[param].view(cube_dim[0] * cube_dim[1], cube_dim[2])
+                    grad = torch.unsqueeze(learner.param_state[param].grad, 0)
+                    grad = torch.unsqueeze(grad, 1).expand(learner_batch_size, -1, -1, -1)
+                    learner.metadata[param] = torch.cat((learner.metadata[param], grad), dim=1)
+                    cube = learner.metadata[param].size()
+                    learner.metadata[param] = learner.metadata[param].view(cube[0] * cube[2] * cube[3], cube[1])
                 all_metadata = torch.cat(list(learner.metadata.values()), dim=0)
-                # print(all_metadata.size())
                 metadata_size = all_metadata.size()[0]
-                # trol = time.time()
                 try:
                     sample_idx = np.random.choice(metadata_size, meta_batch_size, replace=False)
                     sampled_metadata = all_metadata[sample_idx, :]
@@ -87,6 +92,7 @@ class Vanilla(MetaFramework):
                 meta_loader = torch.utils.data.DataLoader(dataset=metadata_from_forward,
                                                           batch_size=meta_batch_size,
                                                           shuffle=True)
+                # backprop error to metalearner with metadataset
                 for j, (triplets, grads) in enumerate(meta_loader):
                     tock = time.time()
                     triplets = Variable(triplets)
@@ -94,19 +100,20 @@ class Vanilla(MetaFramework):
 
                     # Forward + Backward + Optimize
                     meta_optimizer.zero_grad()  # zero the gradient buffer
-                    meta_outputs = meta_weight(triplets)
+                    aug_triplets = torch.unsqueeze(torch.unsqueeze(triplets, 2), 3)
+                    meta_outputs = torch.squeeze(learner.get_update(aug_triplets))
                     meta_loss = meta_criterion(meta_outputs, grads)
-                    # print("Meta-Loss:" + str(meta_loss))
+                    # print("Meta-Loss:" + str(meta_loss.data[0]))
                     meta_loss.backward()
                     meta_optimizer.step()
                     # print(time.time() - tock)
                 # print("ONE FULL PASS")
                 # print(time.clock() - tick)
                 #
-                if (i + 1) % 100 == 0:
-                    print('Epoch [%d/%d], Step [%d/%d], Loss: %.4f'
-                          % (meta_epoch + 1, MetaFramework.num_epochs, i + 1, len(train_dataset) // learner_batch_size, learner_loss.data[0]))
-                    print('Epoch [%d], Loss: %.4f' % (meta_epoch, learner_loss.data[0]))
+                # if (i + 1) % 100 == 0:
+                #     print('Epoch [%d/%d], Step [%d/%d], Loss: %.4f'
+                #           % (meta_epoch + 1, MetaFramework.num_epochs, i + 1, len(train_dataset) // learner_batch_size, learner_loss.data[0]))
+                #     print('Epoch [%d], Loss: %.4f' % (meta_epoch, learner_loss.data[0]))
 
         # Test the Model
         correct = 0
@@ -130,6 +137,6 @@ vanilla_params_init = {'mid1': [400, 20], 'mid2': [200, 20],
                        'learning_rate': [0.0001, 0.00093], 'update_rate': [0.0001, 0.00087]}
 
 vanilla_frame = Vanilla('vanilla', vanilla_fixed_params, vanilla_params_range, vanilla_params_init)
-vanilla_frame.train_model(400, 200, 10, 3000, 0.001, 0.0001)
+vanilla_frame.train_model(400, 200, 10, 3000, 0.001, 0.0001, 10)
 # vanilla_frame.optimize(1)
 # vanilla_frame.analyze()
--- net_components.py
+++ net_components.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as f
 import torchvision.datasets as dsets
 import torchvision.transforms as transforms
 from torch.autograd import Variable
@@ -17,7 +18,6 @@ def timeit(method):
         else:
             print('%r  %2.2f ms' % (method.__name__, (te - ts) * 1000))
         return result
-
     return timed
 
 
@@ -41,7 +41,7 @@ class Meta(nn.Module):
 # Template for Single Structure
 class SingleNet(nn.Module):
 
-    def __init__(self, input_size, hidden1, hidden2, output_size, meta_weight, batch_size):
+    def __init__(self, input_size, hidden1, hidden2, output_size, meta_input, meta_hidden, meta_output, batch_size):
         super(SingleNet, self).__init__()
         self.relu = nn.ReLU()
         self.fc1 = nn.Linear(input_size, hidden1)
@@ -49,22 +49,27 @@ class SingleNet(nn.Module):
         self.fc3 = nn.Linear(hidden2, output_size)
         self.batch_size = batch_size
         self.impulse = None
-        self.meta_weight = meta_weight
+        self.conv1 = nn.Conv2d(in_channels=meta_input, out_channels=meta_hidden, kernel_size=1, bias=True)
+        self.conv2 = nn.Conv2d(in_channels=meta_hidden, out_channels=meta_output, kernel_size=1, bias=True)
         self.metadata = {}
 
         self.param_state = self.state_dict(keep_vars=True)
         keys = list(self.param_state.keys())
 
         def is_weight_param(param):
-            return (".weight" in param) and ("meta" not in param)
+            return (".weight" in param) and ("meta" not in param) and ("conv" not in param)
 
         self.weight_params = [key for key in keys if is_weight_param(key)]
 
     # get new weight
-    def get_update(self, v_i, w_ij, v_j):
-        inputs = Variable(torch.Tensor([v_i, w_ij, v_j]), requires_grad=True)
-        # print("break")
-        return self.meta_weight.forward(inputs).data[0]
+    def get_update(self, meta_input_stack):
+        # print("BEGINNING UPDATE")
+        out = self.conv1(meta_input_stack)
+        # print("First conv:", out.size())
+        out = self.conv2(out)
+        # print("Second conv:", out.size())
+        out = torch.squeeze(out, 1)
+        return out
 
     def forward(self, x):
         self.metadata = {}
@@ -81,12 +86,9 @@ class SingleNet(nn.Module):
         self.impulse.append(out)
         return out
 
-    # @timeit
-    # compute output and propagate Hebbian updates
-    # takes roughly 10 ms per
     def update(self, rate, epoch, change_weights=True):
         # print(weight_params)
-        if len(self.weight_params) != len(self.impulse) - 1:
+        if len(self.weight_params) != len(self.impulse) - 1:  # LHS: learner param layers, RHS: intermediate outputs
             print("Keys:" + str(len(self.weight_params)))
             print(self.weight_params)
             print("Impulse:" + str(len(self.impulse)))
@@ -100,32 +102,21 @@ class SingleNet(nn.Module):
             # print(input_layer.size())
             # print(output_layer.size())
             # print(layer.size())
-            # stack_dim = self.batch_size, layer.size()[0], layer.size()[1]
-            # input_stack = input_layer.unsqueeze(1).expand(stack_dim)
-            # output_stack = output_layer.unsqueeze(2).expand(stack_dim)
-            # weight_stack = layer.unsqueeze(0).expand(stack_dim)
-            # meta_inputs = torch.stack((input_stack, weight_stack, output_stack), dim=3)
-            # print(meta_inputs.size())
-
-            input_stack = input_layer.repeat(output_layer.size(1), 1)
-            output_stack = output_layer.repeat(input_layer.size(1), 1).t()
-            meta_inputs = torch.stack((input_stack, layer, output_stack), dim=2)
+            stack_dim = self.batch_size, layer.size()[0], layer.size()[1]
+            input_stack = input_layer.unsqueeze(1).expand(stack_dim)
+            output_stack = output_layer.unsqueeze(2).expand(stack_dim)
+            weight_stack = layer.unsqueeze(0).expand(stack_dim)
+            meta_inputs = torch.stack((input_stack, weight_stack, output_stack), dim=3)
+            meta_inputs = meta_inputs.permute(0, 3, 1, 2)
             self.metadata[self.weight_params[i]] = meta_inputs
-            # layer.data = torch.stack([self.meta_weight() for i, x_i in enumerate(torch.unbind(x, dim=0), 0)], dim=0)
-            # TODO: vectorize with apply_()
+            # print(meta_inputs.size())
             if change_weights:
-                for input_index in range(0, len(input_layer)):
-                    for output_index in range(0, len(output_layer)):
-                        # print(input_layer.data)
-                        input_to_neuron = input_layer.data[0, input_index]
-                        # print(input_to_neuron)
-                        output_from_neuron = output_layer.data[0, output_index]
-                        # print(output_from_neuron)
-                        neuron_weight = layer.data[output_index, input_index]
-                        # print(neuron_weight)
-                        shift = self.get_update(input_to_neuron, neuron_weight, output_from_neuron)
-                        # print(new_weight)
-                        layer.data[output_index, input_index] -= shift * rate / epoch
+                shift = self.get_update(meta_inputs)
+                clipped_shift = torch.clamp(shift, -1000000, 1000000)
+                batch_shift = torch.mean(clipped_shift, 0)
+                layer.data += batch_shift.data * rate / epoch
+            # raise ValueError("check")
+            # print("finished")
 
 
 # Template for Single Structure
@@ -229,3 +220,119 @@ class SupervisedNet(nn.Module):
         out = self.fc2(out)
         out = self.relu(out)
         return out
+
+
+# Template for Single Structure
+class SingleNetOld(nn.Module):
+
+    def __init__(self, input_size, hidden1, hidden2, output_size, meta_weight, batch_size):
+        super(SingleNetOld, self).__init__()
+        self.relu = nn.ReLU()
+        self.fc1 = nn.Linear(input_size, hidden1)
+        self.fc2 = nn.Linear(hidden1, hidden2)
+        self.fc3 = nn.Linear(hidden2, output_size)
+        self.batch_size = batch_size
+        self.impulse = None
+        self.meta_weight = meta_weight
+        self.metadata = {}
+
+        self.param_state = self.state_dict(keep_vars=True)
+        keys = list(self.param_state.keys())
+
+        def is_weight_param(param):
+            return (".weight" in param) and ("meta" not in param)
+
+        self.weight_params = [key for key in keys if is_weight_param(key)]
+
+    # get new weight
+    def get_update(self, v_i, w_ij, v_j):
+        print(v_i.data)
+        print(w_ij)
+        print(v_j)
+        inputs = torch.Tensor([v_i, w_ij, v_j])
+        inputs = Variable(inputs, requires_grad=True)
+        print("break")
+        return self.meta_weight.forward(inputs).data[0]
+
+    def get_update2(self, vec):
+        return self.meta_weight.forward(vec).data[0]
+
+    def forward(self, x):
+        self.metadata = {}
+        out = x
+        self.impulse = [out]
+        out = self.fc1(out)
+        out = self.relu(out)
+        self.impulse.append(out)
+        out = self.fc2(out)
+        out = self.relu(out)
+        self.impulse.append(out)
+        out = self.fc3(out)
+        out = self.relu(out)
+        self.impulse.append(out)
+        return out
+
+    # @timeit
+    # compute output and propagate Hebbian updates
+    # takes roughly 10 ms per
+    def update(self, rate, epoch, change_weights=True):
+        # print(weight_params)
+        if len(self.weight_params) != len(self.impulse) - 1:
+            print("Keys:" + str(len(self.weight_params)))
+            print(self.weight_params)
+            print("Impulse:" + str(len(self.impulse)))
+            print(self.impulse)
+            raise ValueError("Num keys not 1 less than num impulses")
+        for i in range(0, len(self.weight_params)):
+            layer = self.param_state[self.weight_params[i]]
+            # print(layer)
+            input_layer = self.impulse[i]
+            output_layer = self.impulse[i + 1]
+            print(input_layer.size())
+            print(output_layer.size())
+            print(layer.size())
+            stack_dim = self.batch_size, layer.size()[0], layer.size()[1]
+            input_stack = input_layer.unsqueeze(1).expand(stack_dim)
+            output_stack = output_layer.unsqueeze(2).expand(stack_dim)
+            weight_stack = layer.unsqueeze(0).expand(stack_dim)
+            meta_inputs = torch.stack((input_stack, weight_stack, output_stack), dim=3)
+            print(meta_inputs.size())
+            tick = time.time()
+
+            meta_outputs = torch.zeros(meta_inputs.size()[0], meta_inputs.size()[1], meta_inputs.size()[2])
+            if change_weights:
+                ### iteration
+                for batch_num in range(0, meta_inputs.size()[0]):
+                    for input_index in range(0, len(input_layer)):
+                        for output_index in range(0, len(output_layer)):
+                            v_i = meta_inputs[batch_num, input_index, output_index, 0]
+                            w_ij = meta_inputs[batch_num, input_index, output_index, 1]
+                            v_j = meta_inputs[batch_num, input_index, output_index, 2]
+                            meta_outputs[batch_num, input_index, output_index] = self.get_update(v_i, w_ij, v_j)
+                            print("got here")
+                ### unbind -> apply -> stack // reduction op // module
+                # torch.stack([function(x_i, other_input[i]) for i, x_i in enumerate(torch.unbind(x, dim=axis), 0)], dim=axis)
+            meta_outputs = torch.mean(meta_outputs, 0)
+            print(meta_outputs.size())
+            print(time.time() - tick)
+            raise ValueError("lol")
+
+            # input_stack = input_layer.repeat(output_layer.size(1), 1)
+            # output_stack = output_layer.repeat(input_layer.size(1), 1).t()
+            # meta_inputs = torch.stack((input_stack, layer, output_stack), dim=2)
+            # self.metadata[self.weight_params[i]] = meta_inputs
+            # layer.data = torch.stack([self.meta_weight() for i, x_i in enumerate(torch.unbind(x, dim=0), 0)], dim=0)
+            # # TODO: vectorize with apply_()
+            # if change_weights:
+            #     for input_index in range(0, len(input_layer)):
+            #         for output_index in range(0, len(output_layer)):
+            #             # print(input_layer.data)
+            #             input_to_neuron = input_layer.data[0, input_index]
+            #             # print(input_to_neuron)
+            #             output_from_neuron = output_layer.data[0, output_index]
+            #             # print(output_from_neuron)
+            #             neuron_weight = layer.data[output_index, input_index]
+            #             # print(neuron_weight)
+            #             shift = self.get_update(input_to_neuron, neuron_weight, output_from_neuron)
+            #             # print(new_weight)
+            #             layer.data[output_index, input_index] -= shift * rate / epoch
