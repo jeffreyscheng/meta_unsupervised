14c1af9d8284a0d4fed41731829b5f58d249907b
Jeffrey Cheng <jeffch@wharton.upenn.edu>
2018-04-19 21:47:00 -0400

    update for gpu run

--- control.py
+++ control.py
@@ -139,7 +139,7 @@ class Control(MetaFramework):
 control_fixed_params = {'meta_input': 3, 'meta_output': 1, 'input_size': 784, 'num_classes': 10}
 control_params_range = {'mid1': (20, 800), 'mid2': (20, 800), 'meta_mid': (2, 10), 'meta_batch_size': (1, 10000),
                         'learning_rate': (0.000001, 0.001), 'update_rate': (0.000001, 0.001),
-                        'learner_batch_size': (1, 100)}
+                        'learner_batch_size': (1, 50)}
 control_params_init = {'mid1': [400, 20], 'mid2': [200, 20],
                        'meta_mid': [5, 10], 'meta_batch_size': [100, 2337],
                        'learning_rate': [0.0001, 0.00093], 'update_rate': [0.0001, 0.00087],
