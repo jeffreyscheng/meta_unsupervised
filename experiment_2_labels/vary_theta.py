a4e3884524398b33d606510324a5ec9b1c9d761f
Jeffrey Cheng <jeff.s.cheng@gmail.com>
2018-07-19 00:56:54 -0400

    uncommented

--- experiment_2_labels/vary_theta.py
+++ experiment_2_labels/vary_theta.py
@@ -9,14 +9,14 @@ fn = os.path.join(os.path.dirname(__file__), 'raw_theta_experiment.csv')
 
 acc_dict = []
 
-# for theta in np.arange(0.001, 0.011, 0.001):
-#     acc_dict += run_theta_phi_pair(1, theta)
-#
-# for theta in np.arange(0.01, 0.105, 0.005):
-#     acc_dict += run_theta_phi_pair(1, theta)
-#
-# for theta in np.arange(0.10, 1.01, 0.01):
-#     acc_dict += run_theta_phi_pair(1, theta)
+for theta in np.arange(0.001, 0.011, 0.001):
+    acc_dict += run_theta_phi_pair(1, theta)
+
+for theta in np.arange(0.01, 0.105, 0.005):
+    acc_dict += run_theta_phi_pair(1, theta)
+
+for theta in np.arange(0.10, 1.01, 0.01):
+    acc_dict += run_theta_phi_pair(1, theta)
 
 acc_df = pd.DataFrame(acc_dict)
 try:
