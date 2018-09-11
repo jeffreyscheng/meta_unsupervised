813a24035b65117009317185729c6652740cacef
Jeffrey Cheng <jeff.s.cheng@gmail.com>
2018-09-11 02:46:35 -0400

    commented some stuff out

--- experiment_3_rule_form/create_meta_dataset.py
+++ experiment_3_rule_form/create_meta_dataset.py
@@ -191,16 +191,16 @@ class WritableHebbianFrame(MetaFramework):
                     print(learner.impulse[layer_name].size())
 
                     # samples for metadata_df
-                    batch = [random.randint(0, meta_stack_size[0] - 1) for _ in range(WritableHebbianFrame.num_samp)]
-                    i = [random.randint(0, meta_stack_size[3] - 1) for _ in range(WritableHebbianFrame.num_samp)]
-                    j = [random.randint(0, meta_stack_size[2] - 1) for _ in range(WritableHebbianFrame.num_samp)]
-
-                    def label_tuples(t):
-                        return {'v_i': t[0].data, 'w_ij': t[1].data, 'v_j': t[2].data, 'grad': t[3].data}
-
-                    samples = [label_tuples(learner.impulse[layer_name][batch[x], :, j[x], i[x]])
-                               for x in range(WritableHebbianFrame.num_samp)]
-                    metadata_df = pd.concat([metadata_df, pd.DataFrame(samples)])
+                    # batch = [random.randint(0, meta_stack_size[0] - 1) for _ in range(WritableHebbianFrame.num_samp)]
+                    # i = [random.randint(0, meta_stack_size[3] - 1) for _ in range(WritableHebbianFrame.num_samp)]
+                    # j = [random.randint(0, meta_stack_size[2] - 1) for _ in range(WritableHebbianFrame.num_samp)]
+                    #
+                    # def label_tuples(t):
+                    #     return {'v_i': t[0].data, 'w_ij': t[1].data, 'v_j': t[2].data, 'grad': t[3].data}
+                    #
+                    # samples = [label_tuples(learner.impulse[layer_name][batch[x], :, j[x], i[x]])
+                    #            for x in range(WritableHebbianFrame.num_samp)]
+                    # metadata_df = pd.concat([metadata_df, pd.DataFrame(samples)])
                     # print(metadata_df)
                     del meta_stack_size, layer_grad, samples, batch, i, j
                     gc.collect()
