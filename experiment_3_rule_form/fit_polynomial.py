fac6df0e647332ae27034b0acbd2fdc95651ecdd
Jeffrey Cheng <jeff.s.cheng@gmail.com>
2018-10-25 13:49:22 -0400

    ran experiment 3

--- experiment_3_rule_form/fit_polynomial.py
+++ experiment_3_rule_form/fit_polynomial.py
@@ -33,21 +33,6 @@ num_models = len([name for name in os.listdir(metalearner_directory)
                   if os.path.isfile(os.path.join(metalearner_directory, name))])
 
 
-class LinearRegressionModel(nn.Module):
-
-    def __init__(self, input_dim, output_dim):
-        super(LinearRegressionModel, self).__init__()
-        # Calling Super Class's constructor
-        self.linear = nn.Linear(input_dim, output_dim, bias=False)
-        # nn.linear is defined in nn.Module
-
-    def forward(self, x):
-        # Here the forward pass is simply a linear function
-
-        out = self.linear(x)
-        return out
-
-
 # turns a combination selection of indices into the balls and urns representation (last element is unused)
 def comb_to_sb(tup):
     return tup[0], tup[1] - tup[0] - 1, tup[2] - tup[1] - 1
@@ -59,7 +44,7 @@ def comb_to_sb(tup):
 
 list_of_degree_dfs = []
 
-for d in range(1, 10):  # d is max degree of polynomial
+for d in range(0, 5):  # d is max degree of polynomial
     print("starting degree " + str(d))
     all_combinations = list(itertools.combinations(range(d + 3), 3))
     all_deg_arr = [comb_to_sb(c) for c in all_combinations]
@@ -104,55 +89,23 @@ for d in range(1, 10):  # d is max degree of polynomial
     # num_models = 5 # for testing, remove
     for i in range(num_models):
         model = torch.load(metalearner_directory + os.sep + str(i) + '.model')
-        model_update_ser = deg_appx_train_df.apply(lambda row: model.get_single_update((row['(1, 0, 0)'],
-                                                                                        row['(0, 1, 0)'],
-                                                                                        row['(0, 0, 1)'])), axis=1)
-        # print(model_update_ser)
-        # print(deg_appx_train_df)
-        # print(deg_appx_model_df)
-        input_dim = len(deg_appx_train_df.columns)
-        regression = LinearRegressionModel(input_dim, 1)
-
-        criterion = nn.MSELoss()  # Mean Squared Loss
-        l_rate = 0.01
-        optimiser = torch.optim.SGD(regression.parameters(), lr=l_rate)  # Stochastic Gradient Descent
-
-        epochs = 20000
-        error = 1
-        for epoch in range(epochs):
-            epoch += 1
-            # increase the number of epochs by 1 every time
-            inputs = Variable(torch.Tensor(deg_appx_train_df.values))
-            labels = Variable(torch.Tensor(model_update_ser.values))
-
-            # clear grads as discussed in prev post
-            optimiser.zero_grad()
-            # forward to get predicted values
-            outputs = regression.forward(inputs)
-            loss = criterion(outputs, labels)
-            error = float(loss.data)
-            loss.backward()  # back props
-            optimiser.step()  # update the parameters
-            # print('epoch {}, loss {}'.format(epoch, loss.data[0]))
-        # print("model " + str(i) + ", error " + str(error))
-        # print(regression.parameters())
-        # for name, param in regression.named_parameters():
-            # if param.requires_grad:
-                # print(name, param.data)
-        regression_param = torch.squeeze(dict(regression.named_parameters())['linear.weight'].data)
-        # print(regression_param)
-        # print(regression_param.numpy())
-        # print(tuple(regression_param.numpy()))
-        # print(tuple(list(regression_param.numpy()) + [error]))
-        list_of_regression_params.append(list_to_row_dict(list(regression_param.numpy()) + [error]))
-        # raise ValueError("finished with first model")
-
-        # list_of_regression_params.append(regression.parameters())
-        ## fix regression.parameters()
+        if d == 0:
+            model_update_ser = metadata_df.apply(lambda row: model.get_single_update((row['v_i'], row['w_ij'], row['v_j'])), axis=1)
+        if d > 0:
+            model_update_ser = deg_appx_train_df.apply(lambda row: model.get_single_update((row['(1, 0, 0)'],
+                                                                                            row['(0, 1, 0)'],
+                                                                                            row['(0, 0, 1)'])), axis=1)
+        y = model_update_ser.values
+        # print(y)
+        x = deg_appx_train_df.values
+        regression_solution = np.matmul(np.matmul(np.linalg.inv(np.matmul(x.transpose(), x)), x.transpose()), y)
+        error = np.square(y - np.matmul(x, regression_solution)).mean()
+        print(error)
+        list_of_regression_params.append(list_to_row_dict(list(regression_solution) + [error]))
 
     deg_appx_model_df = deg_appx_model_df.append(list_of_regression_params, ignore_index=True)
     # print(deg_appx_model_df)
-    print(deg_appx_model_df['error'].mean())
+    print("Average error for degree " + str(d) + ": " + str(deg_appx_model_df['error'].mean()))
     d_model_df_path = os.path.join(deg_appx_model_path, str(d) + '.csv')
     deg_appx_model_df.to_csv(d_model_df_path)
     list_of_degree_dfs.append(deg_appx_model_df)
