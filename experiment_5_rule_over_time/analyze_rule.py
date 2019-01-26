79547f5ac0192ad2150ae771f6945dd27c352b1a
Jeffrey Cheng <jeff.s.cheng@gmail.com>
2019-01-26 03:00:06 -0500

    started analyze model

---
+++ experiment_5_rule_over_time/analyze_rule.py
@@ -0,0 +1,20 @@
+import torch
+import os
+
+temp_directory = os.path.join(os.sep.join(os.path.dirname(__file__).split(os.sep)[:-1]),
+                              'temp_data')
+metalearner_directory = os.path.join(temp_directory, 'metalearners_over_time')
+
+if not os.path.exists(temp_directory):
+    os.makedirs(temp_directory)
+if not os.path.exists(metalearner_directory):
+    os.makedirs(metalearner_directory)
+
+num_models = len([name for name in os.listdir(metalearner_directory)
+                  if os.path.isfile(os.path.join(metalearner_directory, name))])
+
+for i in range(num_models):
+    model = torch.load(metalearner_directory + os.sep + str(i) + '.model')
+    print("loaded model")
+    print(model(0, 0, 0))
+    del model
