1390478058bd2801834656e6fa4fc678d0425351
Jeffrey Cheng <jeff.s.cheng@gmail.com>
2019-01-27 00:50:39 -0500

    tried new dimension

--- experiment_5_rule_over_time/analyze_rule.py
+++ experiment_5_rule_over_time/analyze_rule.py
@@ -18,5 +18,5 @@ num_models = len([name for name in os.listdir(metalearner_directory)
 for i in range(num_models):
     model = torch.load(metalearner_directory + os.sep + str(i) + '.model')
     print("loaded model")
-    print(model(Variable(torch.Tensor([0, 0, 0]).unsqueeze(dim=0).unsqueeze(dim=0).unsqueeze(dim=0))))
+    print(model(Variable(torch.zeros([20, 3, 6, 7]))))
     del model
