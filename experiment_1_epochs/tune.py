7f44054edb52a007640c9d4c16fc43e97b8a4c27
Jeffrey Cheng <jeff.s.cheng@gmail.com>
2019-02-09 19:12:20 -0500

    tune

--- experiment_1_epochs/tune.py
+++ experiment_1_epochs/tune.py
@@ -1,7 +1,8 @@
 from experiment_0_util.run_experiment import *
+import numpy as np
 
 print("begin architecture")
-for phi in range(10):
+for phi in range(1, 50):
     print(phi)
-    control_acc = control_frame.train_model(theta=1, phi=phi)
+    control_acc = np.mean([control_frame.train_model(theta=1, phi=phi / 10) for _ in range(10)])
     print(control_acc)
