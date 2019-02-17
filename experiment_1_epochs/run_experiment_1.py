5c668706b9965fded202d6659ec21b859bf63018
Jeffrey Cheng <jeff.s.cheng@gmail.com>
2019-02-17 03:25:36 -0500

    changed exp 1 phi values

--- experiment_1_epochs/run_experiment_1.py
+++ experiment_1_epochs/run_experiment_1.py
@@ -7,14 +7,20 @@ experiment_1_data_path = os.path.join(final_data_path, 'experiment_1_data.csv')
 
 acc_dict = []
 
-for phi in np.arange(0.01, 0.11, 0.01):
+# for phi in np.arange(0.01, 0.10, 0.01):
+#     acc_dict += run_theta_phi_pair(1, phi)
+#
+# for phi in np.arange(0.10, 0.5, 0.05):
+#     acc_dict += run_theta_phi_pair(1, phi)
+
+for phi in np.arange(0.50, 1.0, 0.02):  # this is the critical point
     acc_dict += run_theta_phi_pair(1, phi)
 
-for phi in np.arange(0.10, 1.01, 0.05):
-    acc_dict += run_theta_phi_pair(1, phi)
-
-for phi in np.arange(1.0, 5, 0.5):
-    acc_dict += run_theta_phi_pair(1, phi)
+# for phi in np.arange(1.0, 2, 0.1):
+#     acc_dict += run_theta_phi_pair(1, phi)
+#
+# for phi in np.arange(2.0, 3, 0.2):
+#     acc_dict += run_theta_phi_pair(1, phi)
 
 acc_df = pd.DataFrame(acc_dict)
 try:
