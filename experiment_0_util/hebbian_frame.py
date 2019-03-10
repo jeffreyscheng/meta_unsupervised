0410ae2d2694e211700e5961b5ad876d1025e7a4
Jeffrey Cheng <jeff.s.cheng@gmail.com>
2019-03-10 17:04:05 -0400

    sizing

--- experiment_0_util/hebbian_frame.py
+++ experiment_0_util/hebbian_frame.py
@@ -3,7 +3,43 @@ from hyperparameters import *
 import torch
 import torch.nn as nn
 import gc
+
 import sys
+from numbers import Number
+from collections import Set, Mapping, deque
+
+try:  # Python 2
+    zero_depth_bases = (basestring, Number, xrange, bytearray)
+    iteritems = 'iteritems'
+except NameError:  # Python 3
+    zero_depth_bases = (str, bytes, Number, range, bytearray)
+    iteritems = 'items'
+
+
+def getsize(obj_0):
+    """Recursively iterate to sum size of object & members."""
+    _seen_ids = set()
+
+    def inner(obj):
+        obj_id = id(obj)
+        if obj_id in _seen_ids:
+            return 0
+        _seen_ids.add(obj_id)
+        size = sys.getsizeof(obj)
+        if isinstance(obj, zero_depth_bases):
+            pass  # bypass remaining control flow and return
+        elif isinstance(obj, (tuple, list, Set, deque)):
+            size += sum(inner(i) for i in obj)
+        elif isinstance(obj, Mapping) or hasattr(obj, iteritems):
+            size += sum(inner(k) + inner(v) for k, v in getattr(obj, iteritems)())
+        # Check for custom object instances - may subclass above too
+        if hasattr(obj, '__dict__'):
+            size += inner(vars(obj))
+        if hasattr(obj, '__slots__'):  # can have __slots__ with __dict__
+            size += sum(inner(getattr(obj, s)) for s in obj.__slots__ if hasattr(obj, s))
+        return size
+
+    return inner(obj_0)
 
 
 # Template for Single Structure
@@ -47,10 +83,11 @@ class HebbianNet(nn.Module):
 
         def get_size(x):
             return sum([sys.getsizeof(y.data) for y in x])
+
         batch, channel, layer1, layer2 = meta_input_stack.size()
-        print("size ", get_size(meta_input_stack))
+        print("size ", getsize(meta_input_stack))
         meta_input_stack = meta_input_stack.contiguous().view(batch, channel, layer1 * layer2)
-        print("size ", get_size(meta_input_stack))
+        print("size ", getsize(meta_input_stack))
         meta_input_stack = self.relu(self.conv1(meta_input_stack))
         meta_input_stack = torch.squeeze(self.conv2(meta_input_stack), 1)
         meta_input_stack = meta_input_stack.view(batch, layer1, layer2)
