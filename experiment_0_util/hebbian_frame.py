0cd73615a7e1fb517d109b1c34ca8ca88c961916
Jeffrey Cheng <jeff.s.cheng@gmail.com>
2019-03-10 17:56:44 -0400

    refactored learner fwd

--- experiment_0_util/hebbian_frame.py
+++ experiment_0_util/hebbian_frame.py
@@ -59,7 +59,12 @@ class LearnerNet(nn.Module):
                         print(type(obj), obj.size())
             stack_prod = stack_dim[0] * stack_dim[1] * stack_dim[2]
             meta_inputs = torch.stack((input_stack, weight_stack, output_stack), dim=3).permute(1, 2, 3, 0).contiguous().view(3, stack_prod)
-            shift = torch.stack([self.metalearner(Variable(x_i)) * self.rate / batch_num for i, x_i in enumerate(torch.unbind(x, dim=1), 0)], dim=0).squeeze(0)
+
+            def test_fn(x_i):
+                print(x_i)
+                print(self.metalearner(x_i))
+                return self.metalearner(x_i) * self.rate / batch_num
+            shift = torch.stack([test_fn(x_i) for _, x_i in enumerate(torch.unbind(x, dim=1), 0)], dim=0).squeeze(0)
 
             # output, update weights
             print(old_vj.size())
